dbd125519b9ae52245b2332a53f2383704786939
Karol <cg@local.gitea>
2025-10-08 11:56:18 +0100

    working state

--- examples/widget-gallery/src/tabs.rs
+++ examples/widget-gallery/src/tabs.rs
@@ -4,6 +4,7 @@ use floem::peniko::color::Hsl;
 use floem::prelude::palette::css;
 use floem::prelude::palette::css::BLACK;
 use floem::prelude::palette::css::DARK_GRAY;
+use floem::prelude::palette::css::WHITE;
 use floem::prelude::*;
 use floem::reactive::create_effect;
 use floem::style;
@@ -60,7 +61,7 @@ pub fn tab_view() -> impl IntoView {
         dyn_stack(
             move || tabs.get(),
             |tab| tab.idx,
-            move |tab| tab_side_view(tab.name).on_click_stop(move |_| {
+            move |tab| tab_side_item(tab.clone(), active_tab).on_click_stop(move |_| {
                 active_tab.update(|a| {*a = Some(tab.idx);});
             })
         ).style(|s| s
@@ -87,6 +88,7 @@ pub fn tab_view() -> impl IntoView {
         .width(120.)
         .min_width(120.)
         .height_full()
+        .background(BG)
         .border_right(1.)
         .border_color(css::BLACK)
     );
@@ -99,20 +101,44 @@ pub fn tab_view() -> impl IntoView {
             |tab| tab.idx,
             move |tab| show_tab_content(tab)
         ).style(|s| s.size_full()),
-    )).style(|s| s.size_full());
-    
+    )).style(|s| s.size_full().background(BG));
+
     v_stack((
         h_stack((
             button("add tab").action(move || tab_action.update(|a| {*a = Action::Add;}))
                 .style(|s| s
-                    .background(hsl([0., 0., 0.]))
+                    .background(BG_LIGHT)
+                    .border(0.)
+                    .border_bottom(0.5)
+                    .border_top(0.5)
+                    .border_right(0.)
+                    .border_left(0.)
+                    .border_top_color(WHITE)
+                    .border_bottom_color(BG_DARK)
+                    .box_shadow_blur(1.)
+                    .box_shadow_v_offset(2.)
+                    .box_shadow_color(BG_DARK)
                 ),
-            button("remove tab").action(move || tab_action.update(|a| {*a = Action::Remove;})),
+            button("remove tab").action(move || tab_action.update(|a| {*a = Action::Remove;}))
+                .style(|s| s
+                    .background(BG_LIGHT)
+                    .border(0.)
+                    .border_bottom(0.5)
+                    .border_top(0.5)
+                    .border_right(0.)
+                    .border_left(0.)
+                    .border_top_color(WHITE)
+                    .border_bottom_color(BG_DARK)
+                    .box_shadow_blur(1.)
+                    .box_shadow_v_offset(2.)
+                    .box_shadow_color(BG_DARK)
+                ),
         )).style(|s| s
             .height(40.px())
             .width_full()
             .border_bottom(1.)
             .border_color(css::BLACK)
+            .background(BG)
             .padding(5.)
             .col_gap(5.)
             .items_center()
@@ -123,10 +149,12 @@ pub fn tab_view() -> impl IntoView {
         )).style(|s| s
             .height(300.px())
             .width(400.px())
+            .background(BG)
         ),
     )).style(|s| s
         .size_full()
         .border(1.)
+        .background(BG_DARK)
         .border_radius(6.)
         .border_color(BLACK)
     )
@@ -143,20 +171,49 @@ fn show_tab_content(tab: TabContent) -> impl IntoView {
     )
 }
 
-fn tab_side_view(name: String) -> impl IntoView {
-    text(name).style(|s| s
+fn tab_side_item(tab: TabContent, act_tab: RwSignal<Option<usize>>) -> impl IntoView {
+    text(tab.name).style(move |s| s
         .items_center()
         .justify_center()
         .width_full()
         .height(36.px())
+        .background(BG_LIGHT)
         .border(0.5)
         .border_radius(5.)
-        .hover(|s| s.apply_class(ListItemClass))
+        .hover(|s| s.border_color(BG_DARK).background(BG))
+        .apply_if(
+            act_tab.get().is_some_and(|a| a == tab.idx),
+            |s| s.background(BG_DARK).border_color(BG_LIGHT)
+        )
     )
 }
 
-fn hsl(c: [f32; 3]) -> Color {
-    let hsl = Hsl::to_linear_srgb([c[0], c[1], c[2]]);
-    let rgb = Color::new([hsl[0], hsl[1], hsl[2], 1.]);
+
+const BG_DARK: Color = hsl([0., 0., 90.]);
+const BG: Color = hsl([0., 0., 95.]);
+const BG_LIGHT: Color = hsl([0., 0., 100.]);
+
+const TEXT: Color = hsl([0., 0., 5.]);
+const TEXT_MUTED: Color = hsl([0.0, 0., 30.]);
+
+const fn hsl([h, s, l]: [f32; 3]) -> Color {
+    let sat = s * 0.01;
+    let light = l * 0.01;
+    let a = sat * light.min(1.0 - light);
+   
+    let hh = transform(0., [h, s, l]);
+    let ss = transform(8., [h, s, l]);
+    let ll = transform(4., [h, s, l]);
+    let rgb = Color::new([hh, ss, ll, 1.]);
     rgb
+}
+
+const fn transform(n: f32, [h, s, l]: [f32; 3]) -> f32 {
+    let sat = s * 0.01;
+    let light = l * 0.01;
+    let a = sat * light.min(1.0 - light);
+    
+    let x = n + h * (1.0 / 30.0);
+    let k = x - 12.0 * (x * (1.0 / 12.0)).floor();
+    light - a * (k - 3.0).min(9.0 - k).clamp(-1.0, 1.0)
 }
